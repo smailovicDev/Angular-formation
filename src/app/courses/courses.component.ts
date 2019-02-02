6f0e285fa81d994b74b67f9842909126a78ffc8d
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2019-02-02 17:04:57 +0100

    first day

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,16 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent  {
-   courses = [
-    {id: 1, label: 'Angular'},
-    {id: 2, label: 'JAVA EE'},
-    {id: 3, label: 'Laravel'},
+  
+  editable = false;
+
+  myImage = "https://images.pexels.com/photos/17679/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500"; 
+  
+  courses = [
+    {id: 1, label: 'Angular', active: true},
+    {id: 2, label: 'JAVA EE', active: true},
+    {id: 3, label: 'Laravel', active: false},
+    {id: 1, label: 'Angular', active: true},
   ]
 
 
   myCourse = {
     id: this.courses.length + 1,
-    label: ""
+    label: "",
+    active: false
   };
 
  
@@ -24,6 +31,33 @@ export class CoursesComponent  {
   
   addCourse() {
     this.courses.push(this.myCourse);
+    this.myCourse = {
+      id: this.courses.length + 1,
+      label: "",
+      active: false
+    };
+  }
+
+  DeleteCourse(index) {
+   this.courses.splice(index, 1);
+  }
+
+  editCourse(course) {
+    this.myCourse = course
+    this.editable = true;
+  }
+
+  updateCourse() {
+    this.editable = false;
+    this.myCourse = {
+      id: this.courses.length + 1,
+      label: "",
+      active: false
+    };
+  }
+
+  activeCourse(course) {
+    course.active = !course.active
   }
 
 }
